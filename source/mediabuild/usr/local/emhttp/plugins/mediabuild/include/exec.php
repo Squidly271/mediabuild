e5edea3923958fc8ab6baca07c5b84dbab95482b
Squidly271 <azawadzki@arrowgames.com>
2016-02-08 19:03:14 -0500

    initial demo

---
+++ source/mediabuild/usr/local/emhttp/plugins/mediabuild/include/exec.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Now downloading what ever it was you clicked on<br><br>";
+
+sleep(8);
+
+echo "Verifying md5 of download...";
+
+sleep(5);
+
+echo "OK<br><br>";
+
+echo "Installing to flash drive...";
+
+sleep(2);
+
+echo "OK<br><br>";
+
+echo "Verifying md5 on flash drive...";
+
+sleep(5);
+
+echo "OK<br><br>";
+
+echo "<font size='3' color='red'>You must restart your server to complete the installation</font>";
+
+
+?>
